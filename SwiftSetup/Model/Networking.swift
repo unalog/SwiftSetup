a07a9e5f230d112c7da0e3d51adf1fc07f8c6e04
una <una@ubivelox.com>
2017-11-30 17:09:06 +0900

    setup cocoapod

---
+++ SwiftSetup/Model/Networking.swift
@@ -0,0 +1,13 @@
+//
+//  Networking.swift
+//  SwiftSetup
+//
+//  Created by una on 2017. 11. 30..
+//  Copyright © 2017년 una. All rights reserved.
+//
+
+import UIKit
+
+class Networking: NSObject {
+
+}
