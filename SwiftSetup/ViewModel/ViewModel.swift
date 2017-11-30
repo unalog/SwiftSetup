ecb12f5f29a4d7e22842c970c0261aa18512fc7b
una <una@ubivelox.com>
2017-11-30 17:10:23 +0900

    setup swift

---
+++ SwiftSetup/ViewModel/ViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ViewModel.swift
+//  SwiftSetup
+//
+//  Created by una on 2017. 11. 30..
+//  Copyright © 2017년 una. All rights reserved.
+//
+
+import UIKit
+
+class ViewModel: NSObject {
+
+}
